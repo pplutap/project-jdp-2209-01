a25bf3aaffee5d6770f7c89bf6d5a25a66db0dbf
Kamil Janusz <kamiljanusztc@gmail.com>
2022-08-18 19:25:21 +0200

    Change controller Order

--- src/main/java/com/kodilla/ecommercee/controllers/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controllers/OrderController.java
@@ -2,6 +2,7 @@ package com.kodilla.ecommercee.controllers;
 
 import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,34 +15,45 @@ import java.util.List;
 @RequestMapping("api/v1/orders")
 public class OrderController {
 
-    private static final List<OrderDto> orders = Arrays.asList(
-            new OrderDto(1L,"Order1", "Description1"),
-            new OrderDto(2L,"Order2", "Description3"),
-            new OrderDto(3L,"Order3", "Description4")
+    private final OrderMapper orderMapper;
+    private static final List<Order> orderList = Arrays.asList(
+            new Order(1,"Order1", "Description1"),
+            new Order(2,"Order2", "Description3"),
+            new Order(3,"Order3", "Description4")
     );
+
+    @Autowired
+    public OrderController(OrderMapper taskMapper) {
+        this.orderMapper = taskMapper;
+    }
 
     @GetMapping
     public ResponseEntity<List<OrderDto>> getAllOrders() {
-        return ResponseEntity.ok(orders);
+        return ResponseEntity.ok(orderMapper.mapToTaskDtoList(orderList));
     }
 
     @GetMapping(value = "{orderId}")
     public ResponseEntity<OrderDto> getOrder(@PathVariable Long orderId) {
-        return ResponseEntity.ok(orders.get(0));
+        return ResponseEntity.ok(orderMapper.mapToOrderDto(orderList.get(0)));
     }
 
     @DeleteMapping(value = "{orderId}")
     public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
+        orderList.remove(0); //next: get method from DBservice
         return ResponseEntity.ok().build();
     }
 
     @PutMapping
     public ResponseEntity<OrderDto> updateOrder(@RequestBody OrderDto orderDto) {
-        return ResponseEntity.ok().build();
+        Order order = orderMapper.mapToOrder(orderDto);
+        Order savedOrder = order; //next: get method from DBservice
+        return ResponseEntity.ok(orderMapper.mapToOrderDto(savedOrder));
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createOrder(@RequestBody OrderDto orderDto) {
+        Order order = orderMapper.mapToOrder(orderDto);
+        //next: get method from DBservice
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -1,34 +1,34 @@
-//package com.kodilla.ecommercee.mapper;
-//
-//import com.kodilla.ecommercee.domain.Order;
-//import com.kodilla.ecommercee.domain.OrderDto;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Service
-//public class OrderMapper {
-//
-//    public Order mapToOrder(final OrderDto orderDto) {
-//        return new Order(
-//                orderDto.getId(),
-//                orderDto.getTitle(),
-//                orderDto.getContent()
-//        );
-//    }
-//
-//    public OrderDto mapToOrderDto(final Order order) {
-//        return new OrderDto(
-//                order.getId(),
-//                order.getTitle(),
-//                order.getContent()
-//        );
-//    }
-//
-//    public List<OrderDto> mapToTaskDtoList(final List<Order> orderList) {
-//        return orderList.stream()
-//                .map(this::mapToOrderDto)
-//                .collect(Collectors.toList());
-//    }
-//}
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.OrderDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderMapper {
+
+    public Order mapToOrder(final OrderDto orderDto) {
+        return new Order(
+                orderDto.getId(),
+                orderDto.getTitle(),
+                orderDto.getContent()
+        );
+    }
+
+    public OrderDto mapToOrderDto(final Order order) {
+        return new OrderDto(
+                order.getId(),
+                order.getTitle(),
+                order.getContent()
+        );
+    }
+
+    public List<OrderDto> mapToTaskDtoList(final List<Order> orderList) {
+        return orderList.stream()
+                .map(this::mapToOrderDto)
+                .collect(Collectors.toList());
+    }
+}
