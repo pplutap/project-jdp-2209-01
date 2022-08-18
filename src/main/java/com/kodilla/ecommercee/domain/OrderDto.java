43f69d74380b0a01e18209b9b4fb0a0624d26f57
Kamil Janusz <kamiljanusztc@gmail.com>
2022-08-18 19:38:06 +0200

    Change controller Order

--- src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -1,13 +1,26 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class OrderDto {
 
     private Long id;
     private String title;
     private String content;
+
+    public OrderDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
