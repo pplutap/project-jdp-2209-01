c9805540143fe0fb11a088f682fc7db7e0b9d908
humptydumpty <mateusz.wojtas89@gmail.com>
2022-08-18 13:02:09 +0200

    initial implementation of GroupController

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/groups")
 public class GroupController {
+
+    @GetMapping
+    public ResponseEntity<String> getAllGroups () {
+        return ResponseEntity.ok("List of groups");
+    }
+
+    @GetMapping(value = "{groupId}")
+    public ResponseEntity<String> getGroupById (@PathVariable Long groupId) {
+        return  ResponseEntity.ok("Selected group with id: " + groupId);
+    }
+
+    @DeleteMapping (value = "{groupId}")
+    public ResponseEntity<String> deleteGroupById (@PathVariable Long groupId) {
+        return ResponseEntity.ok("Deleting group by ID: " + groupId);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createNewGroup (@RequestBody String groupDto) {
+        return ResponseEntity.ok("New group created" + groupDto);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> updateGroup (@RequestBody String groupDto) {
+        return ResponseEntity.ok("Group updated" + groupDto);
+    }
+
 }
