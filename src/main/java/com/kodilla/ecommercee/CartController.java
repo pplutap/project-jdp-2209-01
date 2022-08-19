3309f8ad9ee1b15b7f1f398f089dacc524d2ab68
Anna Toczek <ania4134@gmail.com>
2022-08-19 21:19:07 +0100

    InitialInitial implementation of CartController

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/cart")
 public class CartController {
+
+    List<ProductDto> cart;
+
+    @PostMapping
+    public void createCart() {
+        cart = new ArrayList<>();
+    }
+
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public void addProduct(ProductDto productDto) {
+        cart.add(productDto);
+    }
+
+    @DeleteMapping
+    public void deleteProduct(ProductDto productDto) {
+        cart.remove(productDto);
+    }
+
+    @PostMapping
+    public void createOrder(List<ProductDto> cart) {
+
+    }
 }
