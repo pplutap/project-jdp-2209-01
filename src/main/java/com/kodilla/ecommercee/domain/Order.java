3abc7534447b2ab83b40141475a08f2787ad5251
Kamil Janusz <kamiljanusztc@gmail.com>
2022-08-24 22:24:40 +0200

    Update Order class

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -3,21 +3,25 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 @Getter
-@Entity(name = "orders")
+@Entity(name = "ORDERS")
 public class Order {
 
     @Id
     @GeneratedValue
+    @Column(name = "ORDER_ID", unique = true)
     private Long id;
 
     @Column
@@ -26,15 +30,26 @@ public class Order {
     @Column
     private String comment;
 
-    @Column(name = "payment_status")
+    @Column(name = "PAYMENT")
     private boolean isPaid;
 
+    @NotNull
+    @Column(name = "STATUS")
+    private Status status;
+
+    @NotNull
     @CreationTimestamp
     private Date creationDate;
 
-//    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "orders")
-//    public List<Product> getProducts() {
-//        List<Product> products = new ArrayList<>();
-//        return products;
-//    }
+//    @ManyToOne
+//    @JoinColumn(name = "USER_ID")
+//    private User user;
+
+//    @ManyToMany
+//    @JoinTable(
+//            name= "JOIN_ORDERS_PRODUCTS",
+//            joinColumns = {@JoinColumn (name = "ORDER_ID", referencedColumnName = "ORDER_ID")},
+//            inverseJoinColumns = {@JoinColumn (name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
+//    )
+//    private List<ProductRequest> productRequest = new ArrayList<>();
 }
