193986edd5210a5ec702fed4b84b19adee681946
Karol Zmarz <karol.zmarz2@gmail.com>
2022-08-27 11:22:44 +0200

    Encja Product

---
+++ src/main/java/com/kodilla/ecommercee/domain/GroupId.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee.domain;
+
+public class GroupId {
+
+    private Long id;
+    private String name;
+}
--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,9 +1,40 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "products")
 public class Product {
-    private Long id;
+
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "PRODUCT_ID", unique = true)
+    private Long Id;
+
+    @Column(name = "NAME")
     private String name;
+
+    @Column(name = "DESCRIPTION")
     private String description;
-    private Double price;
-    private Long groupId;
+
+    @Column(name = "PRICE")
+    private BigDecimal price;
+
+    @NotNull
+    @Column(name = "GROUP_ID")
+    private GroupId groupId;
+
+    @Column(name = "VERSION")
+    private int version;
+
 }
