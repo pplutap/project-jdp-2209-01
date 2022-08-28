86f5808f6db844de7a64e649cbb70e2451020acc
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-08-28 10:35:23 +0200

    Add interface UserRepository, renamed from UserDTO

--- src/main/java/com/kodilla/ecommercee/controllers/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controllers/UserController.java
@@ -1,33 +1,32 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.domain.UserDTO;
+import com.kodilla.ecommercee.domain.UserDto;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
 
-    private static final List<UserDTO> USERS = Arrays.asList(
-            new UserDTO(1,"Piotr", "1",59403),
-            new UserDTO(2,"Admin", "1",96997),
-            new UserDTO(3,"User", "0",31251)
+    private static final List<UserDto> USERS = Arrays.asList(
+            new UserDto(1,"Piotr", "1",59403),
+            new UserDto(2,"Admin", "1",96997),
+            new UserDto(3,"User", "0",31251)
     );
 
     @GetMapping
-    public ResponseEntity<List<UserDTO>> getAllUsers(){
+    public ResponseEntity<List<UserDto>> getAllUsers(){
         return ResponseEntity.ok(USERS);
     }
 
     @GetMapping(value = "{userId}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable long userId){
+    public ResponseEntity<UserDto> getUserById(@PathVariable long userId){
 
-        return ResponseEntity.ok(new UserDTO(userId, "Test user", "1", 131313));
+        return ResponseEntity.ok(new UserDto(userId, "Test user", "1", 131313));
 
     }
 
