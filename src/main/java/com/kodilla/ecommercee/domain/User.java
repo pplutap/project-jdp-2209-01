f5fa4043d0c4e0ff8463e666ddc55bf6fdeed764
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-08-28 09:38:54 +0200

    Add class User

---
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -0,0 +1,40 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "USER")
+public class User {
+
+    @NotNull
+    @Id
+    @GeneratedValue
+    @Column(name = "USER_ID", unique = true)
+    private long id;
+
+    @Column(name = "USER_NAME")
+    private String name;
+
+    @Column(name = "USER_STATUS")
+    private String status;
+
+    @Column(name = "USER_KEY")
+    private long userKey;
+
+    //@ManyToOne
+    //JoinColumn(name = "LOGIN_ID")
+    //private Login loginInfo;
+
+    //@ManyToOne
+    //JoinColumn(name = "USER_CART")
+    //private Cart cart;
+
+    //@ManyToOne
+    //JoinColumn(name = "USER_ORDER")
+    //private Order order;
+}
