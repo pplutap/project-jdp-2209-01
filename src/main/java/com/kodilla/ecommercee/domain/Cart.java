0397f69e509223918ecf1b5049e12e1051479539
Anna Toczek <ania4134@gmail.com>
2022-08-28 17:18:17 +0100

    CartRepository added

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotNull;
 @Entity(name = "CARTS")
 public class Cart {
 
-    private long id;
+    private Long id;
 //    private User user;
 //    private List<ProductRequest> productRequestList;
 
@@ -19,7 +19,7 @@ public class Cart {
     @NotNull
     @GeneratedValue
     @Column(name = "CART_ID", unique = true)
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
@@ -38,7 +38,7 @@ public class Cart {
 //        return productRequestList;
 //    }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
