cce865358e4a7614e99f6e145f73ff0564262194
Anna Toczek <ania4134@gmail.com>
2022-09-01 20:44:54 +0100

    The last update

--- src/main/java/com/kodilla/ecommercee/repositories/ProductRequestRepository.java
+++ src/main/java/com/kodilla/ecommercee/repositories/ProductRequestRepository.java
@@ -2,6 +2,8 @@ package com.kodilla.ecommercee.repositories;
 
 import com.kodilla.ecommercee.domain.ProductRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRequestRepository extends CrudRepository<ProductRequest, Long> {
 }
