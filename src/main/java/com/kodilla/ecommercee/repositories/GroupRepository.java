24870f34b58f5479f5e65e7e3362568481403fac
humptydumpty <mateusz.wojtas89@gmail.com>
2022-09-08 14:22:19 +0200

    ProductTestSuite

--- src/main/java/com/kodilla/ecommercee/repositories/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repositories/GroupRepository.java
@@ -4,6 +4,9 @@ import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
+    List<Group> findAll ();
 }
--- src/main/java/com/kodilla/ecommercee/repositories/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repositories/ProductRepository.java
@@ -4,6 +4,12 @@ import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    Product findAllById(Long id);
+    List<Product> findAll();
+    void deleteById(Long id);
+
 }
