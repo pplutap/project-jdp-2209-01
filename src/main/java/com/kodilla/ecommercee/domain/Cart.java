5cd841712847c2daa1d2d1c07ade4f18a6ac1d2f
Kamil Janusz <kamiljanusztc@gmail.com>
2022-09-09 09:16:47 +0200

    Update Cart test

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -3,6 +3,7 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
