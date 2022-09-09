4ee7a458745b5f36b6cd039476172124e1a97cab
Anna Toczek <ania4134@gmail.com>
2022-09-09 20:34:51 +0100

    UserTestSuite added

--- src/test/java/com/kodilla/ecommercee/entities/UserTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/entities/UserTestSuite.java
@@ -1,103 +1,118 @@
 package com.kodilla.ecommercee.entities;
 
 import com.kodilla.ecommercee.domain.*;
-import com.kodilla.ecommercee.repositories.CartRepository;
-import com.kodilla.ecommercee.repositories.OrderRepository;
-import com.kodilla.ecommercee.repositories.UserRepository;
+import com.kodilla.ecommercee.repositories.*;
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.sql.Date;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
 public class UserTestSuite {
 
+    private Order order1;
+    private User user;
+    private Group group;
+    private RequestProduct requestProduct;
+    private Product product;
+
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
-    private CartRepository cartRepository;
-
-    @Autowired
     private OrderRepository orderRepository;
 
-//    @Test
-//    public void testUserSaveNoCart() {
-//        //given
-//        //User user = new User(123L, "John Smith", "status", 14523L, Login.LOGGED,null);
-//
-//        //when
-//        userRepository.save(user);
-//        long userId = user.getId();
-//
-//        //then
-//        Assert.assertNotEquals(0, userId);
-//        Assert.assertEquals(123L, userId);
-//
-//        //cleanUp
-//        userRepository.delete(user);
-//    }
-//
-//    @Test
-//    public void testUserSaveWithCart() {
-//        //given
-//        //User user = new User(12L, "John Smith", "status", 14523L, Login.LOGGED,null);
-//        Cart cart = new Cart(145L, user, null);
-//
-//        //when
-//        userRepository.save(user);
-//        cartRepository.save(cart);
-//        long userId = user.getId();
-//        long cartId = cart.getId();
-//
-//        //then
-//        Assert.assertNotEquals(0, userId);
-//        Assert.assertNotEquals(0, cartId);
-//        Assert.assertEquals(12L, userId);
-//        Assert.assertEquals(145L, cartId);
-//
-//        //cleanUp
-//        userRepository.delete(user);
-//        cartRepository.delete(cart);
-//    }
-//
-//    @Test
-//    public void testUserSaveWithOrders() {
-//        //given
-//        List<Order> orders = new ArrayList<>();
-//        User user = new User(12L, "John Smith", "status", 14523L, Login.LOGGED, orders);
-//        Cart cart = new Cart(145L, user, null);
-//        Order order1 = new Order(56L, "order1", "comment1", true, Status.IN_PROGRESS, Date.valueOf(LocalDate.now()), user);
-//        Order order2 = new Order(57L, "order2", "comment2", true, Status.DELIVERED, Date.valueOf(LocalDate.now()), user);
-//        orders.add(order1);
-//        orders.add(order2);
-//
-//        //when
-//        userRepository.save(user);
-//        cartRepository.save(cart);
-//        orderRepository.save(order1);
-//        orderRepository.save(order2);
-//        long userId = user.getId();
-//        long cartId = cart.getId();
-//        long order1Id = order1.getId();
-//        long order2Id = order2.getId();
-//
-//
-//        //then
-//        Assert.assertEquals(12L, userId);
-//        Assert.assertEquals(145L, cartId);
-//        Assert.assertEquals(56L, order1Id);
-//        Assert.assertEquals(57L, order2Id);
-//
-//        //cleanUp
-//        userRepository.delete(user);
-//        cartRepository.delete(cart);
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private RequestProductRepository requestProductRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setObjects() {
+        order1 = new Order();
+        user = new User();
+        group = new Group();
+        product = new Product();
+        requestProduct = new RequestProduct();
+
+        group.setName("Accessories");
+        groupRepository.save(group);
+
+        product.setName("red bag");
+        product.setDescription("Gucci brand");
+        product.setPrice(new BigDecimal(600));
+        product.setGroup(group);
+        product.setVersion(1);
+        productRepository.save(product);
+
+        requestProduct.setProduct(product);
+        requestProduct.setQuantity(1.00);
+        requestProductRepository.save(requestProduct);
+
+        List<Order> orders = new ArrayList<>();
+        orders.add(order1);
+        order1.setCreationDate(new Date(2021, 12,15));
+        order1.setStatus(Status.IN_PROGRESS);
+        order1.setComment("Send asap please");
+        order1.setName("Christmas order");
+        order1.setPaid(true);
+        order1.setRequestProduct(requestProduct);
+        orderRepository.save(order1);
+
+        user.setUserKey(14523L);
+        user.setName("John Smith");
+        user.setStatus("status");
+        user.setLoginInfo(Login.LOGGED);
+        user.setOrders(orders);
+        userRepository.save(user);
+
+        order1.setUser(user);
+    }
+
+    @AfterEach
+    void cleanUp() {
+//        productRepository.delete(product);
+//        groupRepository.delete(group);
+//        requestProductRepository.delete(requestProduct);
 //        orderRepository.delete(order1);
-//        orderRepository.delete(order2);
-//    }
+//        userRepository.delete(user);
+    }
+
+    @Test
+    public void testUserSave() {
+        //given
+
+        //when
+        long userId = user.getId();
+        boolean isExist = userRepository.existsById(userId);
+
+        //then
+        Assert.assertNotEquals(0, userId);
+        Assert.assertEquals(true, isExist);
+    }
+
+    @Test
+    public void testUserDelete() {
+        //given
+
+        //when
+        long userId = user.getId();
+        userRepository.deleteById(userId);
+        boolean isExist = userRepository.existsById(userId);
+
+        //then
+        Assert.assertNotEquals(0, userId);
+        Assert.assertEquals(false, isExist);
+    }
 }
